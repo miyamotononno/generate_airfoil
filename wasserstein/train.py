aec5ec11951ab09068a1f87b3bda93c9fbf449a2
Nozomu Miyamoto <miyamoto-nozomu@g.ecc.u-tokyo.ac.jp>
2020-12-15 23:35:13 +0900

    add wasserstein directory

--- wasserstein/train.py
+++ wasserstein/train.py
@@ -14,16 +14,16 @@ from util import save_loss, to_cpu, save_coords, to_cuda
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--n_epochs", type=int, default=400, help="number of epochs of training")
+parser.add_argument("--n_epochs", type=int, default=1000, help="number of epochs of training")
 parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
 parser.add_argument("--lr", type=float, default=0.00005, help="adam: learning rate")
-parser.add_argument("--latent_dim", type=int, default=15, help="dimensionality of the latent space")
+parser.add_argument("--latent_dim", type=int, default=4, help="dimensionality of the latent space")
 parser.add_argument("--n_classes", type=int, default=1, help="number of classes for dataset")
 parser.add_argument("--coord_size", type=int, default=496, help="size of each image dimension")
 parser.add_argument("--channels", type=int, default=1, help="number of image channels")
 parser.add_argument("--n_critic", type=int, default=5, help="number of training steps for discriminator per iter")
 parser.add_argument("--clip_value", type=float, default=0.01, help="lower and upper clip value for disc. weights")
-parser.add_argument("--sample_interval", type=int, default=1000, help="interval betwen image samples")
+parser.add_argument("--sample_interval", type=int, default=4000, help="interval betwen image samples")
 opt = parser.parse_args()
 # print(opt)
 
@@ -40,8 +40,8 @@ if cuda:
     discriminator.cuda()
 
 # Configure data loader
-perfs_npz = np.load("./dataset/standardized_perfs.npz")
-coords_npz = np.load("./dataset/standardized_coords.npz")
+perfs_npz = np.load("../dataset/standardized_perfs.npz")
+coords_npz = np.load("../dataset/standardized_coords.npz")
 coords = coords_npz[coords_npz.files[0]]
 coord_mean = coords_npz[coords_npz.files[1]]
 coord_std = coords_npz[coords_npz.files[2]]
@@ -74,6 +74,7 @@ def sample_image(epoch=None, data_num=6):
     if epoch is not None:
         save_coords(gen_coords*coord_std+coord_mean, labels, "wasserstein_coord/epoch_{0}".format(str(epoch).zfill(3)))
     else:
+        save_coords(gen_coords*coord_std+coord_mean, labels, "wasserstein_coord/final")
         np.savez("wasserstein_result/final", labels, gen_coords*coord_std+coord_mean)
 
 # ----------
