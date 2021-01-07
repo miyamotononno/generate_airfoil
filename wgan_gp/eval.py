339a77b5ea27cb62d08642a28410e4b39141bfd2
Nozomu Miyamoto <miyamoto-nozomu@g.ecc.u-tokyo.ac.jp>
2021-01-07 16:34:47 +0900

    fix show image

--- wgan_gp/eval.py
+++ wgan_gp/eval.py
@@ -5,7 +5,7 @@ from torch.autograd import Variable
 import torch.nn as nn
 import torch
 from models import Generator, Discriminator
-from util import save_loss, to_cpu, save_coords, to_cuda
+from util import save_loss, to_cpu, save_coords, to_cuda, save_coords_by_cl
 
 cuda = True if torch.cuda.is_available() else False
 FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
@@ -30,19 +30,22 @@ def create_label(data_num, fixed_cl=None):
   labels = Variable(torch.reshape(FloatTensor([labels]), (data_num, 1)))
   return labels
 
+cl_c = 0.6
+
 def sample_image(data_num=12):
     # Sample noise
     z = Variable(FloatTensor(np.random.normal(0, 1, (data_num, latent_dim))))
     # Get labels ranging from 0 to n_classes for n rows
-    labels = create_label(data_num, 0.6)
+    labels = create_label(data_num, cl_c)
     gen_coords = to_cpu(G(z, labels)).detach().numpy()
     labels = to_cpu(labels).detach().numpy()
     # np.savez("results/eval", labels, gen_coords*coord_std+coord_mean)
-    save_coords(gen_coords*coord_std+coord_mean, labels, "coords/eval.png")
+    # save_coords(gen_coords*coord_std+coord_mean, labels, "coords/eval.png")
+    save_coords_by_cl(gen_coords*coord_std+coord_mean, cl_c, "coords/eval.png")
 
 
 G_PATH = "results/generator_params_40000" 
 G = Generator(latent_dim)
 G.load_state_dict(torch.load(G_PATH, map_location=torch.device('cpu')))
 G.eval()
-sample_image()
+sample_image(data_num=20)
