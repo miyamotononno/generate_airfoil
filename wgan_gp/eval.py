deb84b006843631d5acf4434565f2f9953cb902e
Nozomu Miyamoto <miyamoto-nozomu@g.ecc.u-tokyo.ac.jp>
2021-01-13 21:19:47 +0900

    create by cl

--- wgan_gp/eval.py
+++ wgan_gp/eval.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 from models import Generator
 import matplotlib.pyplot as plt
 from calc_cl import get_cl
-from util import save_loss, to_cpu, to_cuda
+from util import to_cpu, to_cuda, save_coords_by_cl
 
 cuda = True if torch.cuda.is_available() else False
 FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
@@ -33,12 +33,13 @@ class Eval:
     labels = np.array([cl_c]*data_num)
     labels = Variable(torch.reshape(FloatTensor([labels]), (data_num, 1)))
     gen_coords = self.rev_standardize(to_cpu(self.G(z, labels)).detach().numpy())
-    return labels, gen_coords
+    return gen_coords
 
   def create_successive_coords(self):
     """0.01から1.50まで151個のC_L^cと翼形状を生成"""
     cl_r = []
     cl_c = []
+    gen_coords = []
     for cl in range(151):
       cl /= 100
       cl_c.append(cl)
@@ -50,9 +51,10 @@ class Eval:
         # cl = 0.1
         if not np.isnan(cl):
           cl_r.append(cl)
+          gen_coords.append(gen_coord)
           break
 
-    return cl_c, cl_r, gen_coord
+    np.savez("results/successive_label", cl_c, cl_r, gen_coords)
 
   def save_coords(self, gen_coords, labels, path):
     data_size = gen_coords.shape[0]
@@ -68,20 +70,29 @@ class Eval:
         ax[i%4, i//4].set_title(title)
 
     fig.savefig(path)
+
+  def successive(self):
+    coords_npz = np.load("results/successive_label.png")
+    cl_c = coords_npz[coords_npz.files[0]]
+    cl_r = coords_npz[coords_npz.files[1]]
+    fig = plt.figure(figsize=(10,5))
+    ax = fig.add_subplot(111)
+    ax.set_xlim([0, 1.5])
+    x = np.linspace(0, 1.5, 10)
+    ax.plot(x, x, color = "black")
+    ax.scatter(cl_c, cl_r)
+    ax.set_xlabel("Specified label")
+    ax.set_ylabel("Recalculated label")
+    # plt.show()
+    fig.savefig("results/successive_label.png")
     
     
 if __name__ == "__main__":
   coords_npz = np.load("../dataset/standardized_coords.npz")
   G_PATH = "results/generator_params_100000"
   evl = Eval(G_PATH, coords_npz)
-  cl_c, cl_r, gen_coords = evl.create_successive_coords()
-  fig = plt.figure(figsize=(10,5))
-  ax = fig.add_subplot(111)
-  ax.set_xlim([0, 1.5])
-  x = np.linspace(0, 1.5, 10)
-  ax.plot(x, x, color = "black")
-  ax.scatter(cl_c, cl_r)
-  ax.set_xlabel("Specified label")
-  ax.set_ylabel("Recalculated label")
-  # plt.show()
-  fig.savefig("results/successive_label.png")
+  cl = [0.0,0.5,1.0,1.5]
+  for cl_c in cl:
+    coords = evl.create_coords_by_cl(cl_c)
+    save_coords_by_cl(coords, str(cl_c), "eval_{0}.png".format(str(cl_c)))
+    
