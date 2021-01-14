3aeb47db340d9f585ef51f9d7be50b4aebcafecf
miyamotononno <miyamoto-nozomu@g.ecc.u-tokyo.ac.jp>
2021-01-14 17:17:16 +0900

    finish learning

--- wgan_gp/train.py
+++ wgan_gp/train.py
@@ -15,7 +15,7 @@ from util import save_loss, to_cpu, save_coords, to_cuda
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--n_epochs", type=int, default=40000, help="number of epochs of training")
+parser.add_argument("--n_epochs", type=int, default=10000, help="number of epochs of training")
 parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
 parser.add_argument("--lr", type=float, default=0.0001, help="adam: learning rate") # 1e-4
 parser.add_argument("--b1", type=float, default=0, help="adam: decay of first order momentum of gradient") # 0.0
@@ -34,7 +34,7 @@ coord_shape = (opt.channels, opt.coord_size)
 cuda = True if torch.cuda.is_available() else False
 lambda_gp = 10
 # Loss weight for gradient penalty
-done_epoch = 0
+done_epoch = 90000
 if done_epoch>0:
     G_PATH = "results/generator_params_{0}".format(done_epoch)
     D_PATH = "results/discriminator_params_{0}".format(done_epoch)
